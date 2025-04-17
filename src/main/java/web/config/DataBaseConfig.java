1695880c867f84ef3300898b94999ee2d47516eb
DANIIL DUDKO <163344188+Oceanologist@users.noreply.github.com>
2025-04-17 07:01:47 +0300

    first commit

---
+++ src/main/java/web/config/DataBaseConfig.java
@@ -0,0 +1,12 @@
+package web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("web.config")
+)
+
+public class DataBaseConfig {
+
+}
